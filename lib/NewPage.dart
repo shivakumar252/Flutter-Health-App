bde2aacc8a399e588ebb59457d944f4110f303e9
shivakumar252 <shivu.mech25@gmail.com>
2021-01-19 07:31:05 -0500

    second

---
+++ lib/NewPage.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
+class NewPage extends StatefulWidget {
+  @override
+  _NewPageState createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  @override
+  Widget build(BuildContext context) {
+//     return MaterialApp(
+// title: (""),
+// debugShowCheckedModeBanner: false,
+// home:Container(
+//         color: Colors.white,
+//         child: CustomPaint(
+//           painter: CurvePainter(),
+//         ),
+//     ),
+ 
+//     );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+              child: Column(children: <Widget>[
+            ClipPath(
+                clipper: WaveClipperOne(flip: false),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 200,
+                      color: Colors.blueGrey,
+                      child: Center(
+                          // child: Text(
+                          //   "Login",
+                          //   style: TextStyle(
+                          //     fontWeight: FontWeight.bold,
+                          //     color: Colors.white,
+                          //     fontSize: 40,
+                          //   ),
+                          // ),
+                          child: Image.network(
+                              "https://png.pngtree.com/thumb_back/fw800/back_our/20190625/ourmid/pngtree-simple-and-flat-medical-security-promotion-background-image_261631.jpg")),
+                    ),
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child:Text("Choose the doctors you want",style:TextStyle(fontSize: 35.0,fontWeight:FontWeight.bold,color:Colors.black))
+                          ),
+                           Container(
+                            child:Text("you can choose your favourite doctors here",style:TextStyle(fontSize: 16.0,fontWeight:FontWeight.bold,color:Colors.black))
+                          ),
+                          // Container(
+                          //   padding: EdgeInsets.all(8.0),
+                          //   child: TextField(
+                          //     decoration: InputDecoration(
+                          //         hintText: "Email or Phone number",
+                          //         hintStyle:
+                          //             TextStyle(color: Colors.grey[500])),
+                          //   ),
+                          // ),
+                          // Container(
+                          //   padding: EdgeInsets.all(8.0),
+                          //   child: TextField(
+                          //     decoration: InputDecoration(
+                          //         hintText: "Password",
+                          //         hintStyle:
+                          //             TextStyle(color: Colors.grey[500])),
+                          //   ),
+                          // ),
+                          SizedBox(
+                           height: 40,
+                          )
+                        ],
+                      ),
+                    ),
+                  
+                    SizedBox(
+                      height: 50,
+                      width: 200,
+                      child:    RaisedButton.icon(
+                      onPressed: () {
+                       
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                      label: Text(
+                        'Get Started',
+                        style: TextStyle(color: Colors.white,fontSize: 18.0),
+                      ),
+                      icon: Icon(
+                        Icons.navigation_sharp,
+                        color: Colors.white,
+                      ),
+                      textColor: Colors.white,
+                      splashColor: Colors.red,
+                      color: Colors.redAccent,
+                    ),
+                    ),
+                 
+                    // Container(
+                    //   height: 50,
+                    //   decoration: BoxDecoration(
+                    //       borderRadius: BorderRadius.circular(10),
+                    //       gradient: LinearGradient(
+                    //           begin: Alignment.centerRight,
+                    //           end: Alignment.centerLeft,
+                    //           colors: [
+                    //             Colors.orange[400],
+                    //             Colors.orange[300]
+                    //           ])),
+                    //   child: Center(
+                    //     child: Text(
+                    //       "Sign Up",
+                    //       style: TextStyle(
+                    //           color: Colors.white, fontWeight: FontWeight.bold),
+                    //     ),
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   height: 20,
+                    // ),
+                    // Container(
+                    //     child: Row(
+                    //   children: <Widget>[
+                    //     Text('Does not have account?'),
+                    //     FlatButton(
+                    //       textColor: Colors.orange[300],
+                    //       child: Text(
+                    //         'Sign Up',
+                    //         style: TextStyle(
+                    //           fontSize: 20,
+                    //         ),
+                    //       ),
+                    //       onPressed: () {
+                    //         //signup screen
+                    //       },
+                    //     )
+                    //   ],
+                    //   mainAxisAlignment: MainAxisAlignment.center,
+                    // )),
+                    // FloatingActionButton(
+                    //   onPressed: null,
+                    //   backgroundColor: Colors.blueAccent,
+                    //   child: Text(
+                    //     "f",
+                    //     style: TextStyle(color: Colors.white, fontSize: 30.0),
+                    //   ),
+                    // )
+                  ],
+                ))
+          ]
+          )
+          ),
+        )
+        );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.orangeAccent;
+    paint.style = PaintingStyle.fill; // Change this to fill
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width / 2, size.height / 2, size.width, size.height * 0.15);
+    path.lineTo(size.width, 0.25);
+    path.lineTo(0, 0.25);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healthy_app/NewPage.dart';
 
 import 'BookSession.dart';
 import 'HomePage.dart';
@@ -18,75 +19,270 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:Color(0xFFFF1744),
+        backgroundColor: Color(0xFFFF1744),
         title: Text("Profile Screen"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children:<Widget> [
-              DrawerHeader(
-      margin: EdgeInsets.zero,
-      padding: EdgeInsets.zero,
-      
-      decoration: BoxDecoration(
-        color: Colors.amber,
-          image: DecorationImage(
-              fit: BoxFit.fill,
-              image:  AssetImage('assets/624111.jpg'))),
-      child: Stack(children: <Widget>[
-        Positioned(
-            bottom: 5.0,
-            left: 16.0,
-            child: Text("SkyCliff IT",
+      ),
+      drawer: Drawer(
+        child: ListView(children: <Widget>[
+          DrawerHeader(
+            margin: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
+            decoration: BoxDecoration(
+                color: Colors.amber,
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage('assets/624111.jpg'))),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                    bottom: 5.0,
+                    left: 16.0,
+                    child: Text("SkyCliff IT",
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                        ))),
+              ],
+            ),
+          ),
+          UserAccountsDrawerHeader(
+            otherAccountsPictures: [],
+            currentAccountPicture: CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGQHa6WkBnTdHFI7ZsODlxNsTSkTht3bMEA&usqp=CAU"),
+            ),
+            accountName: Text(
+              "Shivu",
+              style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold),
+            ),
+            accountEmail: Text(this.email,
                 style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.bold,
-                    ))),
-      ],
-      ),
-      ),
-         UserAccountsDrawerHeader(
-              otherAccountsPictures: [],
-              currentAccountPicture: CircleAvatar(
-                radius: 50.0,
-                backgroundImage: NetworkImage(
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGQHa6WkBnTdHFI7ZsODlxNsTSkTht3bMEA&usqp=CAU"),
+                    fontSize: 14.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold)),
+            arrowColor: Colors.black,
+          ), // DrawerHeader(
+          //   child: Text("Skycliff IT",style: TextStyle(fontWeight:FontWeight.bold,fontSize: 18.0),),
+          //   decoration: BoxDecoration(color: Colors.blue),
+          //   ),
+          // UserAccountsDrawerHeader(
+          //   accountName:Text("Shivu") ,
+          //   accountEmail: Text("Skycliffit@gmail.com")),
+          ListTile(
+            title: Text(
+              "Home",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            leading: Icon(
+              Icons.home,
+              color: Colors.blueAccent,
+            ),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()));
+            },
+          ),
+          ListTile(
+              title: Text(
+                "Your BMI",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold),
               ),
-              accountName: Text("Shivu",style: TextStyle(fontSize:18.0,color:Colors.blueAccent,fontWeight:FontWeight.bold),),
-              accountEmail: Text(this.email,style: TextStyle(fontSize:14.0,color:Colors.black,fontWeight:FontWeight.bold)),
-              arrowColor: Colors.black,
-            ),     // DrawerHeader(
-              //   child: Text("Skycliff IT",style: TextStyle(fontWeight:FontWeight.bold,fontSize: 18.0),),
-              //   decoration: BoxDecoration(color: Colors.blue),
-              //   ),
-                // UserAccountsDrawerHeader(
-                //   accountName:Text("Shivu") ,
-                //   accountEmail: Text("Skycliffit@gmail.com")),
-                ListTile(
-                title: Text("Home",style: TextStyle(color: Colors.black,fontSize: 18.0,fontWeight: FontWeight.bold),),
-                leading: Icon(Icons.home,color: Colors.blueAccent,),
-                onTap: (){
-             Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage()));
-                },
-                ),
-                 ListTile(
-                title: Text("Your BMI",style: TextStyle(color: Colors.black,fontSize: 18.0,fontWeight: FontWeight.bold),),
-                leading: Icon(Icons.event,color: Colors.cyanAccent,),
-                onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>BookSession()));
+              leading: Icon(
+                Icons.event,
+                color: Colors.cyanAccent,
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => BookSession()));
+              }),
+          ListTile(
+            title: Text(
+              "Log Out",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            leading: Icon(
+              Icons.contacts,
+              color: Colors.indigoAccent,
+            ),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Login()));
+            },
+          ),
+        ]),
+      ),
+      body:Container(
+        child:ListTile(
+          title:Text("Hello"),
+          subtitle: OrientationBuilder(
+          builder: (context,orientation){
+            return    GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(4),
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 4,
+        crossAxisCount: 2,
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(10,10,10,10),
+            child: Center(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 60,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Find Doctor",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "210 Doctors",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white60,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.only(top: 20, left: 15),
+                  )
+                ],
+              ),
+            ),
+            color: Colors.red,
+          ),
+        Container(
+            padding: const EdgeInsets.fromLTRB(10,10,10,10),
+            child: Center(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.card_membership,
+                      color: Colors.white,
+                      size: 60,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Appointments",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "50 Appointments Available",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white60,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.only(top: 20, left: 15),
+                  )
+                ],
+              ),
+            ),
+            color: Colors.redAccent,
+          ),
+       Container(
+            padding: const EdgeInsets.fromLTRB(10,10,10,10),
+            child: Center(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.list_alt,
+                      color: Colors.white,
+                      size: 60,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Drug List",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "22 Services",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white60,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.only(top: 20, left: 15),
+                  )
+                ],
+              ),
+            ),
+            color: Colors.grey,
+          ),
+        Container(
+            padding: const EdgeInsets.fromLTRB(10,10,10,10),
+            child: Center(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.medical_services,
+                      color: Colors.white,
+                      size: 60,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      "Find Hospital",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "30 Hospital",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white60,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.only(top: 20, left: 15),
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>(NewPage())));
+                    },
+                  )
+                ],
+              ),
+            ),
+            color: Colors.blueGrey,
+          ),
+        ],
+      );
+          }
+      )
 
-                }
-                ),
-                 ListTile(
-                title: Text("Log Out",style: TextStyle(color: Colors.black,fontSize: 18.0,fontWeight: FontWeight.bold),),
-                leading: Icon(Icons.contacts,color: Colors.indigoAccent,),
-                onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Login()));
-                },
-                )
-            ],),
-        ),
+    
+    )
+    )
     );
   }
 }
